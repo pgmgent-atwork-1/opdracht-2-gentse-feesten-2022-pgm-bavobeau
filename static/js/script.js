50b429652d4953b5b69e3f97e8ba17619c9c6866
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-01-17 19:20:43 +0100

    hamburger menu mobile

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,14 @@
 const menu = document.querySelector(".hamburger_nav");
 const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close_hamburger")
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
     menu.classList.remove("showMenu");
-    hamburger.classList.remove("cross");
   } else {
     menu.classList.add("showMenu");
-    hamburger.classList.add("cross");
   }
 }
 
 hamburger.addEventListener("click", toggleMenu);
+close.addEventListener("click", toggleMenu)
