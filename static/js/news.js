bc3fa9f950802cd9c139855a5ba59e5294837733
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-01-19 17:51:04 +0100

    test

---
+++ static/js/news.js
@@ -0,0 +1,34 @@
+const news = document.querySelector(".news_list");
+
+// fetch data for news
+const fetchDataNews = async () => {
+	const news = await fetch(
+		"https://www.pgm.gent/data/gentsefeesten/news.json"
+	);
+
+	if (news.status === 200) {
+		const data = await news.json();
+		console.log(data);
+		renderNews(data);
+	} else {
+		throw new Error("er is iets misgegaan bij het ophalen van de data");
+	}
+};
+
+const renderNews = (data) => {
+    data.map((news_element) => {
+        let createNews = document.createElement("li");
+        createNews.className = "news_article"
+        createNews.innerHTML = `
+        <a href="news.html" class="news_link">
+            <div class="news_title">
+                <h3>${news_element.title}</h3>
+                <div class="arrow"></div>
+            </div>
+            <img src="https://www.pgm.gent/data/gentsefeesten/${news_element.picture.large}" class="news_image">
+        </a>`
+        news.appendChild(createNews);
+    })
+};
+
+fetchDataNews();
