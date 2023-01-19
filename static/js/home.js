5064f319090f46f685eb59f8bc48f57a25dc6164
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-01-19 17:40:02 +0100

    file changes

--- static/js/home.js
+++ static/js/home.js
@@ -1,5 +1,6 @@
 const events = document.querySelector(".events");
 const event = document.querySelector(".event");
+const news = document.querySelector(".news_items");
 
 //fetching data for events
 const fetchDataEvents = async () => {
@@ -40,4 +41,33 @@ const renderEvents = (event) => {
 	
 };
 
+// fetch data for news
+const fetchDataNews = async () => {
+	const news = await fetch(
+		"https://www.pgm.gent/data/gentsefeesten/news.json"
+	);
+
+	if (news.status === 200) {
+		const data = await news.json();
+		console.log(data);
+		renderNews(data);
+	} else {
+		throw new Error("er is iets misgegaan bij het ophalen van de data");
+	}
+};
+
+const renderNews = (data) => {
+    data.slice(0, 3).map((news_element) => {
+        let createNews = document.createElement("li");
+        createNews.className = "news_article"
+        createNews.innerHTML = `
+        <a href="news.html" class="news_link">
+            <h3>${news_element.title}</h3>
+            <div class="arrow"></div>
+        </a>`
+        news.appendChild(createNews);
+    })
+};
+
 fetchDataEvents();
+fetchDataNews();
