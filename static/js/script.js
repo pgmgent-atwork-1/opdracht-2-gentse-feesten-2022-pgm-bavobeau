d7e75d93eb1adf605690d3fe665ecf179e67ce92
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-01-22 14:18:19 +0100

    day page added toggle list and grid

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector(".hamburger_nav");
 const hamburger = document.querySelector(".hamburger");
-const close = document.querySelector(".close_hamburger")
+const close = document.querySelector(".close_hamburger");
+const grid = document.getElementById("list");
+const grid_button = document.querySelector(".grid");
+const list_button = document.querySelector(".list");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
@@ -10,5 +13,21 @@ function toggleMenu() {
   }
 }
 
+function toggleGrid() {
+  grid.classList.remove("off");
+  grid.classList.add("on");
+  grid_button.classList.add("on");
+  list_button.classList.remove("on");
+}
+
+function toggleList() {
+  grid.classList.add("off");
+  grid.classList.remove("on");
+  list_button.classList.add("on");
+  grid_button.classList.remove("on");
+}
+
 hamburger.addEventListener("click", toggleMenu);
-close.addEventListener("click", toggleMenu)
+close.addEventListener("click", toggleMenu);
+grid_button.addEventListener("click", toggleGrid);
+list_button.addEventListener("click", toggleList);
