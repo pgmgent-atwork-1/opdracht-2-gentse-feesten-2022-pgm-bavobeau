d460f8bc8b23f8abc3cc079e561c13d04523441b
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-01-22 13:25:54 +0100

    home.js uses all events now

--- static/js/home.js
+++ static/js/home.js
@@ -5,18 +5,24 @@ const news = document.querySelector(".news_items");
 //fetching data for events
 const fetchDataEvents = async () => {
 	const events = await fetch(
-		"https://www.pgm.gent/data/gentsefeesten/events_500.json"
-        //"https://www.pgm.gent/data/gentsefeesten/events.json"
+    "https://www.pgm.gent/data/gentsefeesten/events.json"
 	);
 
 	if (events.status === 200) {
 		const data = await events.json();
-		console.log(data);
 		renderEvents(data);
 	} else {
 		throw new Error("er is iets misgegaan bij het ophalen van de data");
 	}
 };
+
+const checkImage = (image) => {
+	if (image != null) {
+	  return image.full;
+	} else {
+	  return "../static/img/bg-twitterfeed.jpg";
+	}
+  };
 
 const renderEvents = (event) => {
 	let i = Math.floor(Math.random() * event.length + 1);
@@ -26,7 +32,7 @@ const renderEvents = (event) => {
 		<li class="event">
 			<a href="#" class="event_link">
 				<div class="event_image">
-					<img src="${event_element.image.full}" class="event_image">
+					<img src="${checkImage(event_element.image)}" class="event_image">
 					<div class="black_field">${event_element.day_of_week.slice(0,2)} ${event_element.day} juli</div>
 				</div>
 				<div class="event_info">
@@ -38,7 +44,7 @@ const renderEvents = (event) => {
 		</li>`
 	}).join("");
 
-	event.innerHTML = html;
+	events.innerHTML = html;
 };
 
 // fetch data for news
@@ -49,7 +55,6 @@ const fetchDataNews = async () => {
 
 	if (news.status === 200) {
 		const data = await news.json();
-		console.log(data);
 		renderNews(data);
 	} else {
 		throw new Error("er is iets misgegaan bij het ophalen van de data");
