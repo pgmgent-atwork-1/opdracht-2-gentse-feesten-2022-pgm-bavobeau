f0d455daff46dd53b749b8b68756273b5faaed20
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-01-22 16:53:49 +0100

    able to render specific event on detail.html

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,15 +1,9 @@
 (() => {
     const $event = document.getElementById("event");
-    let params = new URLSearchParams(location.search);
+    let params = new URLSearchParams(document.location.search);
     let day = params.get('day');
     let slug = params.get('slug'); 
-  
-    const fetchCategories = async () => {
-      let response = await fetch(
-        "https://www.pgm.gent/data/gentsefeesten/categories.json"
-      );
-      return await response.json();
-    };
+    console.log(slug);
   
     const fetchEvents = async () => {
       let response = await fetch(
@@ -20,11 +14,10 @@
   
     const fetchData = async () => {
       try {
-        const categories = await fetchCategories();
         const events = await fetchEvents();
-        console.log(events);
-        renderEvent(events);
-        renderCategories(categories);
+        const active = events.filter(event => event.day === day & event.slug === slug);
+        console.log(active)
+        renderEvent(active);
       } catch (error) {
         document.getElementById("list").innerHTML = error.message;
       }
@@ -32,18 +25,32 @@
   
     fetchData();
 
+    checkContent = (event) => {
+      if (event =! null) {
+        return event;
+      } return "";
+    };
+
     // render one event
-    const renderCategories = (categories) => {
-      const html = categories
-      .map((category) => {
+    const renderEvent = (event) => {
+      const html = event.map((event) => {
         return `
-        <li>
-          <a href="#${category}">
-            ${category}
-          </a>
-        </li>`;
+        <div class="event_main">
+        <h1 class="event_title">${event.title}</h1>
+        <div class="red_field">${event.location}</div>
+        <div class="event_time">${event.start} - ${event.end}</div>
+      </div>
+      <p class="event_desc">${event.description}</p>
+      <ul class="event_info">
+        <li><span>Organisator:</span>${event.organizer}</li>
+        <li><span>Website:</span><a href="${event.url}"></a>${event.url}</li>
+        <li><span>Categorieën:</span><ul>${event.category.map((element) => {return `<li>${element}</li>`})}</ul></li>
+        <li><span>Prijs:</span>${event.ticket}</li>
+        <li><span>Leeftijd:</span>${event.organisator}</li>
+      </ul>
+      `;
       }).join("");
 
       $event.innerHTML = html;
-    };
-  });
+    }; 
+  })();
