ecdac520f550fa229c756da2ca2f703224a8160d
pgm-bavobeau <bavo.beaumon@student.arteveldehs.be>
2023-08-27 19:34:11 +0200

    finished footer

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-// const menu = document.querySelector(".hamburger_nav");
-// const hamburger = document.querySelector(".hamburger");
-// const close = document.querySelector(".close_hamburger");
+const menu = document.querySelector(".hamburger_nav");
+const hamburger = document.querySelector(".header__hamburger");
+const close = document.querySelector(".close_hamburger");
 
-// function toggleMenu() {
-//   if (menu.classList.contains("showMenu")) {
-//     menu.classList.remove("showMenu");
-//   } else {
-//     menu.classList.add("showMenu");
-//   }
-// }
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+  } else {
+    menu.classList.add("showMenu");
+  }
+}
 
-// hamburger.addEventListener("click", toggleMenu);
-// close.addEventListener("click", toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
+close.addEventListener("click", toggleMenu);
